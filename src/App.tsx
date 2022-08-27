3aa1381bc9044e70011ed5889a1e6c8d00283585
fenricage <fenricage@gmail.com>
2022-08-27 23:58:13 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="font-bold text-6xl">hello world</div>
+      </header>
+    </div>
+  )
+}
+
+export default App
