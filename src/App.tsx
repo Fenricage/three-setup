7a7483a656878d7f221978bdb785f3d6a99c55b6
fenricage <fenricage@gmail.com>
2022-08-28 15:02:19 +0300

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -16,43 +16,46 @@ function App() {
       return
     }
 
-    const boxGeometry = new THREE.BoxGeometry(24, 24, 24);
-    const boxMaterial = new THREE.MeshPhongMaterial({color: 0xff0000});
-    const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
-    boxMesh.position.x = -1;
-    project.scene.add(boxMesh);
-
     const gui = new GUI()
 
-    const geometryFolder = gui.addFolder('Mesh Geometry');
-    geometryFolder.open()
+    for (let i = 0; i < 3; i++) {
+      const boxGeometry = new THREE.BoxGeometry(24, 24, 24);
+      const boxMaterial = new THREE.MeshPhongMaterial({color: 0xff0000 * Math.random()});
+      const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
+      boxMesh.position.x = 24 * i * 2;
 
-    const rotationFolder = geometryFolder.addFolder('Rotation')
+      project.scene.add(boxMesh);
 
-    rotationFolder.add(boxMesh.rotation, 'x', 0, Math.PI).name('Rotate X Axis');
-    rotationFolder.add(boxMesh.rotation, 'y', 0, Math.PI).name('Rotate Y Axis');
-    rotationFolder.add(boxMesh.rotation, 'z', 0, Math.PI).name('Rotate Z Axis');
+      const boxFolder = gui.addFolder(`Mesh Box ${i}`)
 
-    const scaleFolder = geometryFolder.addFolder('Scale')
+      const geometryFolder = boxFolder.addFolder(`Mesh Geometry`);
+      geometryFolder.open()
 
-    scaleFolder.add(boxMesh.scale, 'x', 0, 2).name('Scale X Axis');
-    scaleFolder.add(boxMesh.scale, 'y', 0, 2).name('Scale Y Axis');
-    scaleFolder.add(boxMesh.scale, 'z', 0, 2).name('Scale Z Axis');
-    scaleFolder.open();
+      const rotationFolder = geometryFolder.addFolder('Rotation')
 
-    const materialFolder = gui.addFolder('Mesh Material');
+      rotationFolder.add(boxMesh.rotation, 'x', 0, Math.PI).name('Rotate X Axis');
+      rotationFolder.add(boxMesh.rotation, 'y', 0, Math.PI).name('Rotate Y Axis');
+      rotationFolder.add(boxMesh.rotation, 'z', 0, Math.PI).name('Rotate Z Axis');
 
-    const materialParams = {
-      boxMeshColor: boxMesh.material.color.getHex(),
+      const scaleFolder = geometryFolder.addFolder('Scale')
+
+      scaleFolder.add(boxMesh.scale, 'x', 0, 2).name('Scale X Axis');
+      scaleFolder.add(boxMesh.scale, 'y', 0, 2).name('Scale Y Axis');
+      scaleFolder.add(boxMesh.scale, 'z', 0, 2).name('Scale Z Axis');
+      scaleFolder.open();
+
+      const materialFolder = boxFolder.addFolder(`Mesh Material`);
+
+      const materialParams = {
+        boxMeshColor: boxMesh.material.color.getHex(),
+      }
+
+      materialFolder.add(boxMesh.material, 'wireframe');
+
+      materialFolder
+        .addColor(materialParams, 'boxMeshColor')
+        .onChange((value) => boxMesh.material.color.set(value))
     }
-
-    materialFolder.add(boxMesh.material, 'wireframe');
-
-    materialFolder
-      .addColor(materialParams, 'boxMeshColor')
-      .onChange((value) => boxMesh.material.color.set(value))
-
-
   }, [])
 
   return (
