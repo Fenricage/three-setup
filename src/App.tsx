f3bccb78eb53a85dc58115e19ebd0fe6a2e0b435
fenricage <fenricage@gmail.com>
2022-08-28 13:04:01 +0300

    data gui

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react'
 import * as THREE from 'three'
 import SceneInit from './lib/SceneInit';
+import {GUI} from "dat.gui";
 
 function App() {
 
@@ -15,17 +16,33 @@ function App() {
       return
     }
 
-    const boxGeometry = new THREE.BoxGeometry(1, 1, 1, 1, 16);
-    const boxMaterial = new THREE.MeshNormalMaterial({wireframe: true});
+    const boxGeometry = new THREE.BoxGeometry(24, 24, 24);
+    const boxMaterial = new THREE.MeshPhongMaterial({color: 0xff0000});
     const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
     boxMesh.position.x = -1;
     project.scene.add(boxMesh);
 
-    const cylinderGeometry = new THREE.CylinderGeometry(0.5, 0.5, 1, 6, 16);
-    const cylinderMaterial = new THREE.MeshNormalMaterial({wireframe: true})
-    const cylinderMesh = new THREE.Mesh(cylinderGeometry, cylinderMaterial);
-    cylinderMesh.position.x = 1;
-    project.scene.add(cylinderMesh);
+    const gui = new GUI()
+
+    gui.add(boxMesh.rotation, 'x', 0, Math.PI).name('Rotate X Axis');
+    gui.add(boxMesh.rotation, 'y', 0, Math.PI).name('Rotate Y Axis');
+    gui.add(boxMesh.rotation, 'z', 0, Math.PI).name('Rotate Z Axis');
+
+    gui.add(boxMesh.scale, 'x', 0, 2).name('Scale X Axis');
+    gui.add(boxMesh.scale, 'y', 0, 2).name('Scale Y Axis');
+    gui.add(boxMesh.scale, 'z', 0, 2).name('Scale Z Axis');
+
+    const materialParams = {
+      boxMeshColor: boxMesh.material.color.getHex(),
+    }
+
+    gui.add(boxMesh.material, 'wireframe')
+
+    gui
+      .addColor(materialParams, 'boxMeshColor')
+      .onChange((value) => boxMesh.material.color.set(value))
+
+
   }, [])
 
   return (
