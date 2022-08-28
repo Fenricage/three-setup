d8e87b81ab80923586ce2d294e14ab92af3195eb
fenricage <fenricage@gmail.com>
2022-08-28 11:51:11 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,27 @@ import SceneInit from './lib/SceneInit';
 function App() {
 
   useEffect(() => {
-    const test = new SceneInit('myThreeJsCanvas');
-    test.initialize();
-    test.animate();
+    const project = new SceneInit('myThreeJsCanvas');
+    project.initialize();
+    project.animate();
 
-    const boxGeometry = new THREE.BoxGeometry(16, 16, 16);
-    const boxMaterial = new THREE.MeshNormalMaterial();
+
+
+    if(!project.scene) {
+      return
+    }
+
+    const boxGeometry = new THREE.BoxGeometry(1, 1, 1, 1, 16);
+    const boxMaterial = new THREE.MeshNormalMaterial({wireframe: true});
     const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
+    boxMesh.position.x = -1;
+    project.scene.add(boxMesh);
 
-    test.scene.add(boxMesh);
+    const cylinderGeometry = new THREE.CylinderGeometry(0.5, 0.5, 1, 6, 16);
+    const cylinderMaterial = new THREE.MeshNormalMaterial({wireframe: true})
+    const cylinderMesh = new THREE.Mesh(cylinderGeometry, cylinderMaterial);
+    cylinderMesh.position.x = 1;
+    project.scene.add(cylinderMesh);
   }, [])
 
   return (
